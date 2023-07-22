44542ae139340af8dcaae8aab40a9619dcbdbd01
Helena Li <helenali2000@hotmail.com>
2023-07-22 23:37:18 +0200

    part 1 ex 1-24

--- part01-19_food_expenditure/src/food_expenditure.py
+++ part01-19_food_expenditure/src/food_expenditure.py
@@ -1,1 +1,8 @@
 # Write your solution here
+week = int(input("How many times a week do you eat at the student cafeteria? "))
+lunch_price = float(input("The price of a typical student lunch? "))
+groceries = float(input("How much money do you spend on groceries in a week? "))
+
+print("Average food expenditure:")
+print(f"Daily: {((week * lunch_price) + groceries) / 7} euros")
+print(f"Weekly: {groceries + (week * lunch_price) } euros")
--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,4 @@
 # Write your solution here
+students = int(input("How many students on the course? "))
+group_size = int(input("Desired group size? "))
+print(f"Number of groups formed: {students // group_size if students%group_size == 0 else students//group_size + 1}")
